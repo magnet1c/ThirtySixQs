c948ac65f2548476e574c0b162f3768efe28fe54
Andrew Davis <andy.davis543@gmail.com>
2024-08-10 16:38:53 +0100

    Initial Commit

---
+++ ThirtySixQuestions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThirtySixQuestions
+//
+//  Created by Andrew Davis on 10/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThirtySixQuestions/ThirtySixQuestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThirtySixQuestionsApp.swift
+//  ThirtySixQuestions
+//
+//  Created by Andrew Davis on 10/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThirtySixQuestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
